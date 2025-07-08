3feae9a7df58358106b65fc5955697eda9dfd233
Samuel Valentim <134325651+SamValenty@users.noreply.github.com>
2025-07-08 17:43:50 -0300

    Add files via upload

---
+++ Comando de saída.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Esse texto sera escrito na tela \n \n");
+	
+	int x = 10;
+	
+	float y = 20;
+	
+	printf("%d \n \n", x);
+	
+	printf("%d %f \n \n", x, y);
+	
+	printf("O valor de x eh %d e o de y eh %f \n \n", x, y);
+	
+	return 0;
+
+}
+
